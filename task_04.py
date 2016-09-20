da273ff2feb0dd978952f4bc1db9dcb53f6145d7
jchan3 <joechan2014@gmail.com>
2016-09-20 22:38:29 +0000

    commit

---
+++ task_04.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""This module is task_04."""
+
+
+def too_many_kittens(kittens, litterboxes, catfood):
+    """This function determines if there are too many kittens without a
+        litterbox and catfood for each kitten.
+
+    Args:
+        kittenswink(int): Number of kittens.
+        litterboxesnumwink(int): Number of available litterboxes
+        catfood(boolean): Represents whether or not any catfood exists.
+        
+    Returns:
+        boolean: Represents if there is at least one litterbox and some catfood
+        for each kitten.
+
+    Examples:
+
+        >>> too_many_kittens(12, 12, False)
+        True
+
+    """    
+    return not (litterboxes >= kittens and catfood)
