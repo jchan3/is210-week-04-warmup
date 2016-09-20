8e299eedf53fcaac2d8e6009a85ef15448835ff9
jchan3 <joechan2014@gmail.com>
2016-09-20 05:14:23 +0000

    commit

--- task_01.py
+++ task_01.py
@@ -4,6 +4,21 @@
 
 
 def know_what_i_mean(wink, numwink=2):
+    """Adds winks and nudges to end of statement and removes whitespaces.
+
+    Args:
+        wink(mixed): First element at the end of Know what I mean.
+        numwink(int, optional): Number of times to repeat wink. Default: Two
+
+    Returns:
+        str: Statement with winks and nudges repeated at the end.
+
+    Examples:
+
+        >>> know_what_i_mean(';)')
+        'Know what I mean? ;);)nudge nudge'
+
+    """
     winks = (wink * numwink).strip()
     nudges = ('nudge ' * numwink).strip()
     retstr = 'Know what I mean? {}, {}'.format(winks, nudges)
